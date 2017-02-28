696519d82091ac3d408b0395a8651c8e775abd41
Rajan Patel <rpatel3001@gmail.com>
2017-02-27 22:10:05 -0500

    implement jmp

--- vm.c
+++ vm.c
@@ -7,58 +7,35 @@ uint16_t prog[32768];
 
 void execute() {
 	uint16_t IP = 0;
-	uint16_t REG[8];
+	uint16_t REG[8] = {0, 0, 0, 0, 0, 0, 0, 0,};
 	std::stack<uint16_t> STACK;
 
-	while(1) {
+	while (1) {
 		uint16_t op = prog[IP++];
-		if(op == 0) {
+		if (op == 0) {
 			// halt
 			break;
-		} else if(op == 1) {
-			
-		} else if(op == 2) {
-			
-		} else if(op == 3) {
-			
-		} else if(op == 4) {
-			
-		} else if(op == 5) {
-			
-		} else if(op == 6) {
-			
-		} else if(op == 7) {
-			
-		} else if(op == 8) {
-			
-		} else if(op == 9) {
-			
-		} else if(op == 10) {
-			
-		} else if(op == 11) {
-			
-		} else if(op == 12) {
-			
-		} else if(op == 13) {
-			
-		} else if(op == 14) {
-			
-		} else if(op == 15) {
-			
-		} else if(op == 16) {
-			
-		} else if(op == 17) {
-			
-		} else if(op == 18) {
-			
-		} else if(op == 19) {
+		} else if (op == 6) {
+			// jmp
+			uint16_t loc = prog[IP];
+			if (loc > 32775) {
+				std::cout << "Invalid memory address" << std::endl;
+			} else if (loc > 32767) {
+				IP = REG[loc - 32768];
+			} else {
+				IP = loc;
+			}
+		} else if (op == 19) {
 			// print ascii char
 			std::cout << (char)prog[IP++];
-		} else if(op == 20) {
-			
-		} else if(op == 21) {
+		} else if (op == 21) {
 			// noop
 			continue;
+		} else if (op > 21) {
+			std::cout << "Invalid opcode" << std::endl;
+		} else {
+			// not yet implemented
+			std::cout << prog[IP-1] << " " << prog[IP] << " " << prog[IP+1] << " " << prog[IP+2] << std::endl;
 		}
 	}
 	std::cout << "Program finished at instruction " << IP << std::endl;
@@ -68,15 +45,15 @@ int main(int argc, char** argv) {
 	struct stat prog_stat;
 	std::ifstream prog_file;
 
-	if(argc != 2) {
+	if (argc != 2) {
 		std::cout << "Usage: ./vm binary_file" << std::endl;
 		return -1;
 	}
-	if(stat(argv[1], &prog_stat)){
+	if (stat(argv[1], &prog_stat)) {
 		std::cout << "Error stat-ing file" << std::endl;
 		return -1;
 	}
-	if(prog_stat.st_size > 65536) {
+	if (prog_stat.st_size > 65536) {
 		std::cout << "Program size too big" << std::endl;
 		return -1;
 	}
