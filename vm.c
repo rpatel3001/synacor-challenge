2f823945796da3df02e59fa77e035ceba2dee9c2
Rajan Patel <rpatel3001@gmail.com>
2017-02-27 21:54:07 -0500

    second code

--- vm.c
+++ vm.c
@@ -9,6 +9,59 @@ void execute() {
 	uint16_t IP = 0;
 	uint16_t REG[8];
 	std::stack<uint16_t> STACK;
+
+	while(1) {
+		uint16_t op = prog[IP++];
+		if(op == 0) {
+			// halt
+			break;
+		} else if(op == 1) {
+			
+		} else if(op == 2) {
+			
+		} else if(op == 3) {
+			
+		} else if(op == 4) {
+			
+		} else if(op == 5) {
+			
+		} else if(op == 6) {
+			
+		} else if(op == 7) {
+			
+		} else if(op == 8) {
+			
+		} else if(op == 9) {
+			
+		} else if(op == 10) {
+			
+		} else if(op == 11) {
+			
+		} else if(op == 12) {
+			
+		} else if(op == 13) {
+			
+		} else if(op == 14) {
+			
+		} else if(op == 15) {
+			
+		} else if(op == 16) {
+			
+		} else if(op == 17) {
+			
+		} else if(op == 18) {
+			
+		} else if(op == 19) {
+			// print ascii char
+			std::cout << (char)prog[IP++];
+		} else if(op == 20) {
+			
+		} else if(op == 21) {
+			// noop
+			continue;
+		}
+	}
+	std::cout << "Program finished at instruction " << IP << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -26,8 +79,6 @@ int main(int argc, char** argv) {
 	if(prog_stat.st_size > 65536) {
 		std::cout << "Program size too big" << std::endl;
 		return -1;
-	} else {
-		std::cout << "Program is " << prog_stat.st_size << " bytes" << std::endl;
 	}
 	prog_file.open(argv[1], std::ios::in | std::ios::binary);
 	// take advantage of x86 being little endian and read raw bytes into an array of uint16_t
