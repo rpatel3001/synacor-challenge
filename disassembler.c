6fcb88fa155b2b92981d89fff36177d36def46d8
Rajan Patel <rpatel3001@gmail.com>
2017-03-01 15:38:10 -0500

    finish up disassembler

--- disassembler.c
+++ disassembler.c
@@ -3,37 +3,87 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <list>
+#include <map>
 #include <sstream>
+#include <iomanip>
 
 uint16_t prog[32768];
-std::list<std::string> assem;
+std::map<uint16_t, std::string> assem;
+std::map<uint16_t, std::string> funcs;
+std::map<uint16_t, std::string> labels;
 
 int params[] = {0, 2, 1, 1, 3, 3, 1, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 1, 0, 1, 1, 0};
 std::string ops[] = {"halt", "set ", "push", "pop ", "eq  ", "gt  ", "jmp ", "jt  ", "jf  ", "add ", "mult", "mod ", "and ", "or  ", "not ", "rmem", "wmem", "call", "ret ", "out ", "in  ", "noop"};
-void disassemble() {
-	for (size_t i = 0; i < 32768; ++i) {
+void disassemble(size_t size) {
+	size_t func_count = 0;
+	size_t label_count = 0;
+	for (size_t i = 0; i < size; ++i) {
 		std::stringstream sout;
 		uint16_t op = prog[i];
-
+		uint16_t instr = i;
 		if (op > 21) {
 			continue;
 		} else if (op == 19) {
-			sout << "out \"";
-			while (op == 19) {
-				if((char)prog[i+1] == '\n') {
-					sout << "\\n";
+			if (prog[i+1] >= 32768) {
+				sout << "out\treg" << (prog[++i]-32768);
+			} else {
+				sout << "out\t\"";
+				while (op == 19) {
+					if ((char)prog[i+1] == '\n') {
+						sout << "\\n";
+						i += 2;
+						break;
+					}
+					sout << (char)prog[i+1];
 					i += 2;
-					break;
+					op = prog[i];
 				}
-				sout << (char)prog[i+1];
-				i += 2;
-				op = prog[i];
+				--i;
+				sout << "\"";
 			}
-			--i;
-			sout << "\"";
+		} else if (op == 6) {
+			uint16_t addr = prog[++i];
+			sout << "jmp\t";
+			if (labels.find(addr) == labels.end()) {
+				labels[addr] = "label" + std::to_string((label_count++));
+			}
+			sout << labels[addr];
+		} else if (op == 7) {
+			uint16_t test = prog[++i];
+			uint16_t addr = prog[++i];
+			sout << "jt\t";
+			if (test < 32768) {
+				sout << test << "\t";
+			} else {
+				sout << "reg" << (test-32768) << "\t";
+			}
+			if (labels.find(addr) == labels.end()) {
+				labels[addr] = "label" + std::to_string((label_count++));
+			}
+			sout << labels[addr];
+		} else if (op == 8) {
+			uint16_t test = prog[++i];
+			uint16_t addr = prog[++i];
+			sout << "jf\t";
+			if (test < 32768) {
+				sout << test << "\t";
+			} else {
+				sout << "reg" << (test-32768) << "\t";
+			}
+			if (labels.find(addr) == labels.end()) {
+				labels[addr] = "label" + std::to_string((label_count++));
+			}
+			sout << labels[addr];
+		} else if (op == 17) {
+			uint16_t addr = prog[++i];
+			sout << "call\t";
+			if (funcs.find(addr) == funcs.end()) {
+				sout << "func" << (func_count++);
+			} else {
+				sout << funcs[addr];
+			}
 		} else {
-			sout << params[op] << " " << i << " " << ops[op] << "\t";
+			sout << ops[op] << "\t";
 			for (size_t j = 1; j <= params[op]; ++j) {
 				uint16_t val = prog[i+j];
 				if (val < 32768) {
@@ -44,7 +94,7 @@ void disassemble() {
 			}
 			i += params[op];
 		}
-		assem.push_back(sout.str());
+		assem.insert(make_pair(instr, sout.str()));
 	}
 }
 
@@ -70,11 +120,16 @@ int main(int argc, char** argv) {
 	prog_file.read((char*)prog, prog_stat.st_size);
 	prog_file.close();
 
-	disassemble();
+	disassemble(prog_stat.st_size/2);
 
 	std::ofstream outfile(strcat(argv[1], ".asm"), std::ios::out);
-	for (std::string s : assem) {
-		outfile << s << std::endl;
+	for (auto line : assem) {
+		if (labels.find(line.first) != labels.end()) {
+			outfile << labels[line.first] << ":" << std::endl;
+		} else if (funcs.find(line.first) != funcs.end()) {
+			outfile << funcs[line.first] << ":" << std::endl;
+		}
+		outfile << "\t" << line.first << "\t" << line.second << std::endl;
 	}
 	outfile.close();
 }
